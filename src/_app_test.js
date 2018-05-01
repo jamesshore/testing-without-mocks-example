3984f9bb878db5a497334c39e3634118ac07314c
James Shore <jshore@jamesshore.com>
2018-05-01 15:22:15 -0500

    First test

---
+++ src/_app_test.js
@@ -0,0 +1,12 @@
+// Copyright Titanium I.T. LLC.
+"use strict";
+
+const assert = require("./assert.js");
+
+describe("Nothing", function() {
+
+	it("runs tests", function() {
+		assert.todo();
+	});
+
+});
