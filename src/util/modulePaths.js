a57b9f24964f1fba07fe0952c7ca1236f66f6d31
James Shore <jshore@jamesshore.com>
2022-12-25 15:52:33 -0800

    Add 'modulePaths' for calculating filenames relative to a module

---
+++ src/util/modulePaths.js
@@ -0,0 +1,7 @@
+import path  from 'path';
+import { fileURLToPath } from 'url';
+
+export function pathToFile(moduleUrl, relativeFilePath) {
+	const modulePath = fileURLToPath(moduleUrl);
+	return path.resolve(path.dirname(modulePath), relativeFilePath);
+}
