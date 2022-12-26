000f7e7208f76b47435b056cf42fb67e6915b345
James Shore <jshore@jamesshore.com>
2022-12-25 17:38:40 -0800

    Remove obsolete ROT-13 import in run.js

--- src/run.js
+++ src/run.js
@@ -1,8 +1,7 @@
 // Copyright Titanium I.T. LLC.
 
 import CommandLine from "./infrastructure/command_line.js";
-import rot13 from "./logic/rot13.js";
 import App from "./app.js";
 
 const commandLine = CommandLine.create();
-App.create(commandLine, rot13).run();
+App.create(commandLine).run();
