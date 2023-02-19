ecc06676f13dc52a672c0f98148d89322e327057
James Shore <jshore@jamesshore.com>
2023-02-18 23:34:39 -0800

    Add OutputListener, a more ergonomic version of OutputTracker

---
+++ src/infrastructure/_output_listener_test.js
@@ -0,0 +1,56 @@
+// Copyright Titanium I.T. LLC.
+import assert from "../util/assert.js";
+import OutputListener from "./output_listener.js";
+
+describe("OutputListener", () => {
+
+	it("tracks emitted events", () => {
+		const { listener, tracker } = createListener();
+
+		listener.emit("my output 1");
+		listener.emit("my output 2");
+
+		assert.deepEqual(tracker.data, [
+			"my output 1",
+			"my output 2",
+		]);
+	});
+
+	it("supports arbitrary data types", () => {
+		const { listener, tracker } = createListener();
+
+		listener.emit({ data: [ "nested", 3.14 ] });
+		assert.deepEqual(tracker.data, [
+			{ data: [ "nested", 3.14 ] },
+		]);
+	});
+
+	it("can be stopped", () => {
+		const { listener, tracker } = createListener();
+
+		listener.emit("my output 1");
+		tracker.stop();
+		listener.emit("my output 2");
+
+		assert.deepEqual(tracker.data, [ "my output 1" ]);
+	});
+
+	it("can be cleared", () => {
+		const { listener, tracker } = createListener();
+
+		listener.emit("my output 1");
+		assert.deepEqual(tracker.clear(), [ "my output 1" ]);
+
+		listener.emit("my output 2");
+		assert.deepEqual(tracker.clear(), [ "my output 2" ]);
+	});
+
+});
+
+function createListener() {
+	const listener = OutputListener.create();
+	const tracker = listener.trackOutput();
+
+	return { listener, tracker };
+}
+
---
+++ src/infrastructure/output_listener.js
@@ -0,0 +1,56 @@
+// Copyright Titanium I.T. LLC.
+import EventEmitter from "node:events";
+
+const EVENT = "event";
+
+export default class OutputListener {
+
+	static create() {
+		return new OutputListener();
+	}
+
+	constructor() {
+		this._emitter = new EventEmitter();
+	}
+
+	trackOutput() {
+		return new OutputTracker(this._emitter, EVENT);
+	}
+
+	emit(data) {
+		this._emitter.emit(EVENT, data);
+	}
+
+}
+
+
+class OutputTracker {
+
+	static create(emitter, event) {
+		return new OutputTracker(emitter, event);
+	}
+
+	constructor(emitter, event) {
+		this._emitter = emitter;
+		this._event = event;
+		this._data = [];
+
+		this._trackerFn = (text) => this._data.push(text);
+		this._emitter.on(this._event, this._trackerFn);
+	}
+
+	get data() {
+		return this._data;
+	}
+
+	clear() {
+		const result = [ ...this._data ];
+		this._data.length = 0;
+		return result;
+	}
+
+	stop() {
+		this._emitter.off(this._event, this._trackerFn);
+	}
+
+}
